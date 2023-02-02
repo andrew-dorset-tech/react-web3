9185d29155668a4302daae3e22dadb6ee2976af2
andrew-dorset-tech <120407776+andrew-dorset-tech@users.noreply.github.com>
2023-02-02 20:50:10 +0100

    test 123

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
+    }
+  }
 };
